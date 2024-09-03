ef1af7b8bf7b1c788bc6b649f8569bc81d4ef5dd
kejiong <kejiong.li@gmail.com>
2024-09-03 13:54:28 +0100

    correct typo

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -13,7 +13,7 @@ public class App
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
-        System.out.printIn(sayHelloToSomeone("Kejiong"));
+        System.out.println(sayHelloToSomeone("Kejiong"));
         System.out.println(sayGoodbye());
     }
 
