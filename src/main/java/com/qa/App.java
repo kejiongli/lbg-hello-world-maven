c839abb535cc5ab1b5cf2f291986979de27a1fff
kejiong <kejiong.li@gmail.com>
2024-09-03 13:57:36 +0100

    add additional print statement for Ben

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayHelloToSomeone("Kejiong"));
+        System.out.println(sayHelloToSomeone("Ben"));
         System.out.println(sayGoodbye());
     }
 
